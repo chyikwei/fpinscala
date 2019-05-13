05f3dfcc4ae123053dc39780ed357d5b0090e7d5
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2019-05-12 22:20:37 -0700

    getting started done

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -36,7 +36,11 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    if (n == 0) 0
+    else if (n == 1) 1
+    else fib(n-1) + fib(n-2)  
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -140,7 +144,15 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(low: Int, high: Int, gt: (A,A) => Boolean): Boolean = {
+      if (low >= high) true
+      else if (gt(as(low), as(low+1))) false
+      else go(low+1, high, gt)
+    }
+    go(0, as.length - 1, gt)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -153,13 +165,13 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => ((b: B) => f(a, b))
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a: A, b: B) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -172,7 +184,15 @@ object PolymorphicFunctions {
   */
 
   // Exercise 5: Implement `compose`
-
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    (a: A) => f(g(a))
+
+  def main(args: Array[String]): Unit = {
+    assert(isSorted(Array(1,2,3,4), (x:Int, y:Int) => x > y))
+    assert(isSorted(Array(5,1,2,3,4), (x:Int, y:Int) => x > y) == false)
+    assert(partial1(3, (a: Int, b: Int) => a + b)(4) == 7)
+    assert(curry((a: Int, b: Int) => a + b)(3)(4) == 7)
+    assert(uncurry(curry((a: Int, b: Int) => a + b))(3, 4) == 7)
+    assert(compose((a: Int) => a + 3, (b: Int) => b + 4)(1) == 8)
+  }
 }
